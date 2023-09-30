54b420c7337d4b822a0443abd2698c8c75e9894b
Duy Trần Văn Thanh <102210250@sv1.dut.udn.vn>
2023-09-30 16:10:14 +0700

    static file

--- src/server.js
+++ src/server.js
@@ -7,9 +7,13 @@ const app = express();
 const port = process.env.PORT || 3001;
 const hostname = process.env.HOST_NAME || 'localhost';
 
+//config template engine
 //app.set('views','./views');
 app.set('views',path.join(__dirname,'views') );
-app.set('view engine','ejs');
+app.set('view engine','ejs');   
+
+//config static files:image,css,js
+app.use(express.static(path.join(__dirname,'public')));
 
 
 app.get('/',(req,res)=>{
