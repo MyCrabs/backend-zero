3e202c56112c421eabaf7df5cb4cdfc67f3eb3be
Duy Trần Văn Thanh <102210250@sv1.dut.udn.vn>
2023-10-01 17:30:18 +0700

    MVC database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,16 @@
+const connection = require('../config/database');
 const getHomepage = (req,res) => 
 {
-    res.send('Welldone!!! Duyy');
+    let users = [];
+    connection.query(
+        'select * from Users u',
+        function(err, results, fields) {
+          users = results;
+          console.log('>>>"result home page = "',results); // results contains rows returned by server
+          console.log('>>>check users: ',users); 
+          res.send(JSON.stringify(users));
+        }
+      );
 }
 const getABCpage = (req,res) =>
 {
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');//commonjs
 const configviewE = require('./config/viewEnigine');//config template engine
 const webRouter = require('./routes/web');
-const mysql = require('mysql2')
+const connection = require('./config/database');
 
 const app = express();
 const port = process.env.PORT || 3001;  
@@ -11,24 +11,14 @@ const hostname = process.env.HOST_NAME || 'localhost';
 configviewE(app);
 app.use('',webRouter);//Dùng để chia route
 
-//test connection
-const connection = mysql.createConnection(
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT, //default : 3306
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-    }
-);
 //simple querry
-connection.query(
-    'SELECT * FROM Users u',
-    function(err, results, fields) {
-      console.log('>>>"result = "',results); // results contains rows returned by server
-      //console.log(">>>>>fields = ",fields); // fields contains extra meta data about results, if available
-    }
-  );
+// connection.query(
+//     'select * from Users u',
+//     function(err, results, fields) {
+//       console.log('>>>"result = "',results); // results contains rows returned by server
+//       //console.log(">>>>>fields = ",fields); // fields contains extra meta data about results, if available
+//     }
+// );
 
 app.listen(port,hostname, ()=>{
     console.log(`Example listening on port ${port} and ${hostname}`);
