01e4d4b81da9ec40bfd98131f18b4efdab7af4fb
Duy Trần Văn Thanh <102210250@sv1.dut.udn.vn>
2023-10-03 21:39:39 +0700

    form-add

--- src/server.js
+++ src/server.js
@@ -11,15 +11,6 @@ const hostname = process.env.HOST_NAME || 'localhost';
 configviewE(app);
 app.use('',webRouter);//Dùng để chia route
 
-//simple querry
-// connection.query(
-//     'select * from Users u',
-//     function(err, results, fields) {
-//       console.log('>>>"result = "',results); // results contains rows returned by server
-//       //console.log(">>>>>fields = ",fields); // fields contains extra meta data about results, if available
-//     }
-// );
-
 app.listen(port,hostname, ()=>{
     console.log(`Example listening on port ${port} and ${hostname}`);
 })
